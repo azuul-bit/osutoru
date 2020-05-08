fd8d82981b5f8c6608413263d8ae876d6da307f8
azuul-bit <64683861+azuul-bit@users.noreply.github.com>
2020-05-09 00:09:49 +0900

    ちょっとだけ修正
    リファクタリング

--- osutoru/render.js
+++ osutoru/render.js
@@ -24,23 +24,23 @@
         h: 500
     };
     var CELL_SIZE = RECT_CANV.w / COL | 0;
-    var COLOR_LINE = "#FFFFFF";
-    var COLOR_WHITE = "#FFFFFF";
-    var COLOR_BLACK = "#000000";
-    var COLOR_SELECT = "#FFFFFF";
 
-    var COLOR_PANEL_4 = "#006400 ";
-    var COLOR_PANEL_5 = "#03a803 ";
-    var COLOR_PANEL_6 = "#04cb04";
+    var COLOR_BLACK = "#000000";
+    var COLOR_WHITE = "#FFFFFF";
+    var COLOR_RED = "#b22222";
+    var COLOR_YELLOW = "#FFFF99";
+    var COLOR_LINE = "#000000";
+    var COLOR_SELECT = "#F3FFD8";
+    var COLOR_CANDIDATE = "#AAAAAA";
 
     var state_cache = null;
-    var prev_revision = -1;
     var canv_cache = {
         canv_board: null,
         canv_pieaces: null,
         canv_candidate: null,
         canv_effect: null
     };
+    var prev_revision = -1;
 
     //-------------------------------------
     function render(ctx, state, point) {
@@ -90,32 +90,26 @@
         var ctx = canv_cache.canv_board.getContext('2d');
         ctx.clearRect(0, 0, RECT_CANV.w, RECT_CANV.h);
 
-        var grad = ctx.createLinearGradient(0, 0, RECT_CANV.w, RECT_CANV.h);
-        grad.addColorStop(0, COLOR_PANEL_6);
-        grad.addColorStop(0.3, COLOR_PANEL_5);
-        grad.addColorStop(1, COLOR_PANEL_4);
-        ctx.fillStyle = grad
-
         for (var x = 0; x < COL; x++) {
             for (var y = 0; y < COL; y++) {
                 ctx.strokeStyle = COLOR_LINE;
+                ctx.lineWidth = 6;
+                ctx.fillStyle = COLOR_WHITE;
                 ctx.beginPath();
                 ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
                 ctx.strokeRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
             }
         }
-        var canv_board2 = document.createElement("canvas");
-        var ctx_board2 = canv_board2.getContext('2d');
-        canv_board2.width = RECT_CANV.w;
-        canv_board2.height = RECT_CANV.h;
-        ctx_board2.clearRect(0, 0, RECT_CANV.w, RECT_CANV.h);
-        ctx_board2.globalAlpha = 0.07;
-        ctx_board2.fillStyle = COLOR_WHITE;
-        ctx_board2.beginPath();
-        ctx_board2.arc(CELL_SIZE * 1, -3 * CELL_SIZE, 7 * CELL_SIZE, 0, Math.PI * 2, false);
-        ctx_board2.fill();
-        ctx.drawImage(canv_board2, 0, 0, RECT_CANV.w, RECT_CANV.h);
-
+        for (var x = 0; x < COL; x++) {
+            ctx.fillStyle = COLOR_WHITE;
+            ctx.beginPath();
+            ctx.fillRect(45 + x * 100, 0, 10, 500);
+        }
+        for (var y = 0; y < COL; y++) {
+            ctx.fillStyle = COLOR_WHITE;
+            ctx.beginPath();
+            ctx.fillRect(0, 45 + y * 100, 500, 10);
+        }
 
         return canv_cache.canv_board;
     }
@@ -155,25 +149,20 @@
 
         switch (number) {
         case 1:
-            font_color = COLOR_BLACK;
-            fill_color = COLOR_WHITE;
-            grad.addColorStop(0, 'rgb(180, 180, 180)');
-            grad.addColorStop(0.4, fill_color);
-            grad.addColorStop(1, fill_color);
+            font_color = COLOR_WHITE;
+            fill_color = COLOR_RED;
+            ctx.fillStyle = COLOR_RED;
             ctx.fillRect(x + CELL_SIZE / 10, y + CELL_SIZE / 10, CELL_SIZE - 1 * CELL_SIZE / 5, CELL_SIZE - 1 * CELL_SIZE / 5);
             break;
         case -1:
             font_color = COLOR_WHITE;
-            fill_color = COLOR_BLACK;
-            grad.addColorStop(0, fill_color);
-            grad.addColorStop(0.4, 'rgb(70, 70, 70)');
-            grad.addColorStop(1, 'rgb(70, 70, 70)');
+            fill_color = COLOR_YELLOW;
+            ctx.fillStyle = COLOR_YELLOW;
             ctx.fillRect(x + CELL_SIZE / 10, y + CELL_SIZE / 10, CELL_SIZE - 1 * CELL_SIZE / 5, CELL_SIZE - 1 * CELL_SIZE / 5);
             break;
         case 9:
-            fill_color = COLOR_BLACK;
-            grad.addColorStop(0, fill_color);
-            grad.addColorStop(1, 'rgb(70, 70, 70)');
+            fill_color = COLOR_YELLOW;
+            ctx.fillStyle = COLOR_BLACK;
             ctx.arc( x + CELL_SIZE / 2, y + CELL_SIZE / 2, 35, 0 * Math.PI / 180, 360 * Math.PI / 180, false);
             ctx.fill();
             break;
@@ -201,15 +190,11 @@
             var x = (candidateCell[i] % COL | 0) * CELL_SIZE;
             var y = (candidateCell[i] / COL | 0) * CELL_SIZE;
 
-            ctx.globalAlpha = 0.5;
-            ctx.fillStyle = COLOR_SELECT;
+            ctx.globalAlpha = 0.3;
+            ctx.fillStyle = COLOR_CANDIDATE;
             ctx.lineWidth = 1;
             ctx.beginPath();
             ctx.fillRect(x, y, CELL_SIZE, CELL_SIZE);
-            ctx.shadowColor = "rgba(0, 0, 0, 0)";;
-            ctx.shadowBlur = 0;
-            ctx.shadowOffsetX = 0;
-            ctx.shadowOffsetY = 0;
         }
 
         return canv_cache.canv_candidate;
@@ -226,17 +211,13 @@
         var y = (state.pointed / COL | 0) * CELL_SIZE;
 
         ctx.clearRect(0, 0, RECT_CANV.w, RECT_CANV.h);
-        ctx.globalAlpha = 0.5;
+        ctx.globalAlpha = 0.3;
         ctx.fillStyle = COLOR_SELECT;
         ctx.lineWidth = 1;
         ctx.beginPath();
         ctx.fillRect(x, y, CELL_SIZE, CELL_SIZE);
 
         return canv_cache.canv_effect;
-    }
-
-    function objCopy(obj) {
-        return JSON.parse(JSON.stringify(obj));
     }
 
 
